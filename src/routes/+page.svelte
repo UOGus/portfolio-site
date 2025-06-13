ab4fd1a4152d0f0ade9b2e472a9b5fc2bcd6801b
UOGus <166261586+UOGus@users.noreply.github.com>
2025-06-13 12:52:25 -0700

    next

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,13 @@
     <nav class="flex justify-between">
         <h1>GUS COCHRAN</h1>
         <!-- Contacts -->
-        <div class="flex items-center gap-3">
+        <div class="flex items-center gap-7">
+            <h1>HOME</h1>
+            <h1>WORK</h1>
             <!-- Email -->
-            <button aria-label="Copy my Email!" class="cursor-pointer" use:copy={{text: "guscochran77@gmail.com", onCopy(){  }}}>
+            <a aria-label="Copy my Email!" class="cursor-pointer" href="mailto:guscochran77@gmail.com">
                 <i class="fa-solid fa-square-envelope fa-xl"></i>
-            </button>
+            </a>
             
             <!-- Linkedin -->
             <a aria-label="Linkedin" href="https://www.linkedin.com/in/gus-cochran/" target="_blank">
@@ -20,12 +22,12 @@
             </a>
                 
             <!-- Instagram -->
-            <a aria-label="Linkedin" href="https://www.instagram.com/gus.f.c/" target="_blank">
+            <a aria-label="Instagram" href="https://www.instagram.com/gus.f.c/" target="_blank">
                 <i class="fa-brands fa-instagram fa-xl cursor-pointer"></i>
             </a>
             
             <!-- Github -->
-            <a aria-label="Linkedin" href="https://github.com/UOGus" target="_blank">
+            <a aria-label="Github" href="https://github.com/UOGus" target="_blank">
                 <i class="fa-brands fa-square-github fa-xl cursor-pointer"></i>
             </a>
             
